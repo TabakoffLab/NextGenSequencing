9d2859f28501a24c1f015e77cec2358af17bd66b
LauraSaba <Laura.Saba@ucdenver.edu>
2015-01-22 16:54:12 -0700

    from laptop

---
+++ Rat.RI.Brain.totalRNA.batch2/programs/convertToFastQ.batch2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#PBS -l nodes=smp-3-2
+/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch2/alignedReads/BXH2_1.rRNA
+/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch2/alignedReads/HXB1_1.rRNA
+/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch2/alignedReads/HXB3_1.rRNA
+/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch2/alignedReads/HXB4_1.rRNA
+/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch2/alignedReads/HXB5_1.rRNA
+/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch2/alignedReads/HXB10_1.rRNA
+/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch2/alignedReads/HXB15_1.rRNA
+/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch2/alignedReads/HXB18_1.rRNA
+/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch2/alignedReads/HXB20_1.rRNA
+/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch2/alignedReads/HXB21_1.rRNA
+/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch2/alignedReads/HXB22_1.rRNA
+/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch2/alignedReads/HXB23_1.rRNA
+/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch2/alignedReads/HXB24_1.rRNA
+/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch2/alignedReads/HXB29_1.rRNA
+/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch2/alignedReads/HXB31_1.rRNA
+/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch2/alignedReads/SHR_1.rRNA
+
+
+
+
--- Rat.RI.Brain.totalRNA.batch3/programs/convertToFastQ.batch3.sh
+++ Rat.RI.Brain.totalRNA.batch3/programs/convertToFastQ.batch3.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 #PBS -l nodes=smp-3-2
-/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/BNLX_1.rRNA
-/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/BNLX_2.rRNA
-/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/BXH10_1.rRNA
-/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/BXH10_2.rRNA
-/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/BXH11_1.rRNA
-/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/BXH11_2.rRNA
-#/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/BXH13_1.rRNA
-#/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/BXH3_1.rRNA
-#/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/BXH3_2.rRNA
-#/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/BXH5_1.rRNA
-#/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/BXH6_1.rRNA
-#/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/BXH6_2.rRNA
-#/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/BXH8_1.rRNA
-#/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/BXH9_1.rRNA
-#/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/BXH9_2.rRNA
-#/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/SHR_1.rRNA
+/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/alignedReads/BNLX_1.rRNA
+/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/alignedReads/BNLX_2.rRNA
+/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/alignedReads/BXH10_1.rRNA
+/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/alignedReads/BXH10_2.rRNA
+/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/alignedReads/BXH11_1.rRNA
+#/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/alignedReads/BXH11_2.rRNA
+#/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/alignedReads/BXH13_1.rRNA
+#/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/alignedReads/BXH3_1.rRNA
+#/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/alignedReads/BXH3_2.rRNA
+#/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/alignedReads/BXH5_1.rRNA
+#/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/alignedReads/BXH6_1.rRNA
+#/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/alignedReads/BXH6_2.rRNA
+#/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/alignedReads/BXH8_1.rRNA
+#/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/alignedReads/BXH9_1.rRNA
+#/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/alignedReads/BXH9_2.rRNA
+#/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/alignedReads/SHR_1.rRNA
 
 
