a99639f0030b1dbc91a5f4e219f0e00a8ee09e03
Laura Saba <Laura.Saba@ucdenver.edu>
2015-01-22 16:21:15 -0700

    moving to laptop

---
+++ Rat.RI.Brain.totalRNA.batch3/programs/convertToFastQ.batch3.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#PBS -l nodes=smp-3-2
+/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/BNLX_1.rRNA
+/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/BNLX_2.rRNA
+/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/BXH10_1.rRNA
+/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/BXH10_2.rRNA
+/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/BXH11_1.rRNA
+/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/BXH11_2.rRNA
+#/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/BXH13_1.rRNA
+#/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/BXH3_1.rRNA
+#/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/BXH3_2.rRNA
+#/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/BXH5_1.rRNA
+#/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/BXH6_1.rRNA
+#/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/BXH6_2.rRNA
+#/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/BXH8_1.rRNA
+#/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/BXH9_1.rRNA
+#/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/BXH9_2.rRNA
+#/storage/saba/sharedPrograms/createFastQFromBAM.sh /storage/saba/Rat.RI.Brain.totalRNA.batch3/SHR_1.rRNA
+
+
